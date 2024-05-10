e71e8d43a68a263f6c7f1249b933f1b307386b69
Victor Uma <umavictor11@gmail.com>
2024-05-10 17:16:07 +0100

    chore: lint

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,14 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import { useState } from "react";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import { images } from "../../constants";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
   return (
-    <View>
-      <Text>signIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log into Kora
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => {
+              setForm((prevForm) => ({ ...prevForm, email: e }));
+            }}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => {
+              setForm((prevForm) => ({ ...prevForm, password: e }));
+            }}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
 
-export default SignIn
+          <View className="justify-center items-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?{" "}
+            </Text>
+            <Link
+              className="text-lg text-secondary font-semibold"
+              href="/sign-up"
+            >
+              Sign Up
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default SignIn;
+
+const styles = StyleSheet.create({});
--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Image, ScrollView } from 'react-native';
-import { Link } from "expo-router"
+import { Redirect, router } from "expo-router"
 import { SafeAreaView } from 'react-native-safe-area-context'
 import CustomButton from '../components/CustomButton';
 import {images} from '../constants'
@@ -11,7 +11,7 @@ export default function App() {
       <ScrollView contentContainerStyle={{
         height: '100%'
       }}>
-        <View className="w-full items-center h-full px-4">
+        <View className="w-full items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -30,7 +30,7 @@ export default function App() {
           
           {/* <Link href='/home'> link to</Link> */}
           <Text className="font-pregular text-sm text-gray-100 mt-7 text-center"> Where creativity meets innovation. Embark on a mission of limitless opportuinities with Kora</Text>
-          <CustomButton title='Continue with Email' handlePress={() => { }} containerStyles='w-full mt-7'></CustomButton>
+          <CustomButton title='Continue with Email' handlePress={() => {router.push('/sign-in') }} containerStyles='w-full mt-7'></CustomButton>
           <StatusBar backgroundColor='#161622' style='light'/>
         </View>
       </ScrollView>
