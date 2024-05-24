fe0c7c438236c3958372cae9c3af61b2fbb72843
Victor Uma <umavictor11@gmail.com>
2024-05-24 20:15:55 +0100

    "Added useState import and initialized loading state in create component"

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,8 +1,10 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const create = () => {
+  const [loading, isLoading] = useState;
+
   return (
     <SafeAreaView>
       <ScrollView>
